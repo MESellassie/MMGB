8fb78b48cbf5993cf922dc6ad4a93e1b00d78b93
Micah Evans <micah.s.evans@gmail.com>
2020-05-01 18:17:40 -0400

    Finished file structure for Homepage. All pages have been moderalized.

--- mmgb/src/components/NewFiles/pages/Card.js
+++ mmgb/src/components/NewFiles/pages/Card.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Card(props) {
     return (
-        <div className="card mb-3">
+        <div className="card mb-3" id="cardMood">
             <div className="row no-gutters">
                 <div className="col-md-4">
-                    <h5 className="card-title">Mood</h5>
+                    <h5 className="card-title">""</h5>
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-8" id="cardImg">
                     <div className="Card-body">
-                    <img src="..." class="card-img" alt="..."></img>  
+                    <img src="https://github.com/MESellassie/MMGB/blob/master/mmgb/public/BlankImageBox.png?raw=true" class="card-img" alt="..."></img>  
                     </div>
                 </div>
             </div>
--- mmgb/src/components/NewFiles/pages/Sidebar.js
+++ mmgb/src/components/NewFiles/pages/Sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Jumbotron from "./Jumbotron";
 import Row from "./Row";
 import Card from "./Card";
+import Body from "./Body";
 
 function Sidebar(props) {
     return (
@@ -9,11 +10,18 @@ function Sidebar(props) {
             <Row>
                 <div className="col-sm-2 shadow" id="sidebar">
                     <p className="plug">Plug of the Day</p>
+                    <br></br>
+                    <Card></Card>
+                    <br></br>
+                    <Card></Card>
+                    <br></br>
                     <Card></Card>
                     <br></br>
                     <Card></Card>
                     {props.children}
                 </div>
+                <Body></Body>
+                
             </Row>
         </Jumbotron>
     );
