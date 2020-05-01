ed5f7ab404d9094d7c68b9c0e1e74436d0073f89
Micah Evans <micah.s.evans@gmail.com>
2020-05-01 13:59:51 -0400

    Updated file structure once again and reverted to simple html layout for homepage.

--- mmgb/src/App.js
+++ mmgb/src/App.js
@@ -27,10 +27,10 @@
 
 
 import React from "react";
-import OmdbContainer from "./components/OmdbContainer";
+import MMGB from "../src/components/NewFiles/pages/MMGB";
 
 function App() {
-  return <OmdbContainer />;
+  return <MMGB />;
 }
 
 export default App;
