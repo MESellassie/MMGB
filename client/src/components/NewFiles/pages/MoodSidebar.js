75b0a67c3abea7d5048a0c45c50a58bcedfec619
Micah Evans <micah.s.evans@gmail.com>
2020-05-04 18:59:03 -0400

    Added onClick functionality for first Mood result page

---
+++ client/src/components/NewFiles/pages/MoodSidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Jumbotron from "./Jumbotron";
+import Row from "./Row";
+import MoodBody from "./MoodBody";
+
+function MoodSidebar(props){
+    return (
+        <Jumbotron>
+            <Row>
+                <div className="col-sm-2 shadow" id="moodSidebar">
+                    {props.children}
+                </div>
+                <MoodBody></MoodBody>
+            </Row>
+        </Jumbotron>
+    )
+}
+
+export default MoodSidebar;
