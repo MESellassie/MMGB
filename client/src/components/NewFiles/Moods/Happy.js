625059bbda53e2ea9b1d4afcea97c1f0cd90d935
Alan Elam <aelam757@gmail.com>
2020-05-04 20:16:01 -0400

    adding className to the correct hrefs

--- client/src/components/NewFiles/Moods/Happy.js
+++ client/src/components/NewFiles/Moods/Happy.js
@@ -3,7 +3,7 @@ import React from "react";
 function Happy(props) {
     return (
         <button className="btn col-sm-3" type="button" id="happyBtn">
-            <img src="https://github.com/MESellassie/MMGB/blob/master/client/public/images/HappyMood.png?raw=true" class="card-img" alt="..."></img>
+            <img src="https://github.com/MESellassie/MMGB/blob/master/client/public/images/HappyMood.png?raw=true" className="card-img" alt="..."></img>
             {props.children}
         </button>
 
