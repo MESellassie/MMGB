1635066c03f415cd9dcc8a986d195c64818e2456
Micah Evans <micah.s.evans@gmail.com>
2020-05-04 20:30:32 -0400

    HappyResults & Body changes

--- client/src/components/NewFiles/Results/HappyResults.js
+++ client/src/components/NewFiles/Results/HappyResults.js
@@ -1,18 +1,16 @@
 import React from "react";
-import Navbar from "../pages/Navbar";
 import Container from "../pages/Container";
 import MoodSidebar from "../pages/MoodSidebar";
-import Footer from "../pages/Footer";
 
 
 function HappyResults(){
     return (
         <div>
-            <Navbar></Navbar>
+            
             <Container>
                 <MoodSidebar></MoodSidebar>
             </Container>
-            <Footer></Footer>
+
         </div>
     );
 }
