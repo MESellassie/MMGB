0c6f4224622da2d6b13b95d6be5ad4ffe979dc15
Aaron Thomas <acthomas83@gmail.com>
2020-05-07 18:50:07 -0400

    results page initial styling

--- client/src/components/NewFiles/pages/MoodCards.js
+++ client/src/components/NewFiles/pages/MoodCards.js
@@ -1,92 +1,42 @@
 import React from "react";
 import Row from "./Row";
 import Container from "./Container";
-
 function MoodCards() {
     return (
-        <Container>
-        <Row>
-    
-            <div className="row row-cols-4 row-cols-md-2 movieCardResults">
-                <div className="col mb-4">
-                    <div className="moodCard1">
-                        Test
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="col mb-4">
-                    <div className="moodCard2">
-                        Test
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="col mb-4">
-                    <div className="moodCard3">
-                        Test
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="col mb-4">
-                    <div className="moodCard4">
-                        Test
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="col mb-4">
-                    <div className="moodCard4">
-                        Test
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
+    <div className="moodContainer">
+        <div className="movieCardWrapper">MOVIES
+            <div className="movieCardWrapper2">
+                <div className="cardResults" id="movieBox1">MOVIE 1</div>
+                <div className="cardResults" id="movieBox2">MOVIE 2</div>
+                <div className="cardResults" id="movieBox3">MOVIE 3</div>
+                <div className="cardResults" id="movieBox4">MOVIE 4</div>
             </div>
-
-            <div className="row row-cols-4 row-cols-md-2 musicCardResults">
-                <div className="col mb-4">
-                    <div className="moodCard1">
-                        Test 2
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="col mb-4">
-                    <div className="moodCard2">
-                        Test 2
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="col mb-4">
-                    <div className="moodCard3">
-                        Test 2
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="col mb-4">
-                    <div className="moodCard4">
-                        Test 2
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
-                <div className="col mb-4">
-                    <div className="moodCard4">
-                        Test 2
-                        <img src="" className="card-img-top" alt=""></img>
-                    </div>
-                </div>
-
+        </div>
+        <div className="musicCardWrapper">MUSIC
+            <div className="musicCardWrapper2">
+                <div className="cardResults" id="musicBox1">MUSIC 1</div>
+                <div className="cardResults" id="musicBox2">MUSIC 2</div>
+                <div className="cardResults" id="musicBox3">MUSIC 3</div>
+                <div className="cardResults" id="musicBox4">MUSIC 4</div>
             </div>
-
-            </Row>
-
-            </Container>
+        </div>
+        <div className="gamesCardWrapper">GAMES
+            <div className="gamesCardWrapper2">
+                <div className="cardResults" id="gamesBox1">GAMES 1</div>
+                <div className="cardResults" id="gamesBox2">GAMES 2</div>
+                <div className="cardResults" id="gamesBox3">GAMES 3</div>
+                <div className="cardResults" id="gamesBox4">GAMES 4</div>
+            </div>
+        </div>
+        <div className="booksCardWrapper">BOOKS
+            <div className="booksCardWrapper2">
+                <div className="cardResults" id="booksBox1">BOOKS 1</div>
+                <div className="cardResults" id="booksBox2">BOOKS 2</div>
+                <div className="cardResults" id="booksBox3">BOOKS 3</div>
+                <div className="cardResults" id="booksBox4">BOOKS 4</div>
+            </div>
+        </div>
+    </div>
     )
 }
-
 export default MoodCards
