140d4bfadeddfac24a4f4878487eeda43bf4d7b5
Micah Evans <micah.s.evans@gmail.com>
2020-05-08 15:12:18 -0400

    Added files to images folder

--- client/src/components/NewFiles/Moods/MoodSidebar.js
+++ client/src/components/NewFiles/Moods/MoodSidebar.js
@@ -10,7 +10,9 @@ function MoodSidebar(props){
         <Jumbotron>
             <Row>
                 <div className="col-sm-2 shadow" id="moodSidebar">
-                    <div className="moodSidebarTitle">HAPPY</div> 
+                    <div className="moodSidebarTitle">
+                    <img src="" className="https://files.slack.com/files-pri/TQ8JS4T32-F012WCNEH9V/happy.png" alt="..."></img> 
+                    </div>
                     {props.children}
                 </div>
                 <MoodBody></MoodBody>
