fed6f27b152cff4ff953477a08dc2b37873524fe
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-05-12 19:08:55 -0400

    trying spotify again

--- client/src/utils/spodifyAPI.js
+++ client/src/utils/spodifyAPI.js
@@ -1,8 +1,26 @@
 import axios from "axios";
 const BASEURL = "https://www.";
+const APIKEY = ""
 
 export default {
-  random: function() {
-    return axios.get(BASEURL);
+  random: function () {
+      // A list of all characters that can be chosen.
+      const characters = 'abcdefghijklmnopqrstuvwxyz';
+
+      // Gets a random character from the characters string.
+      const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+      let randomSearch = '';
+
+      // Places the wildcard character at the beginning, or both beginning and end, randomly.
+      switch (Math.round(Math.random())) {
+        case 0:
+          randomSearch = randomCharacter + '%';
+          break;
+        case 1:
+          randomSearch = '%' + randomCharacter + '%';
+          break;
+      }
+  
+    return axios.get(BASEURL + randomSearch);
   }
 };
