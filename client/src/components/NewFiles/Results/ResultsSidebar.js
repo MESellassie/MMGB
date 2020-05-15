de263d2c8109cf18182beebcdf6f7354b86b26c4
Aaron Thomas <acthomas83@gmail.com>
2020-05-15 16:39:23 -0400

    results styling

--- client/src/components/NewFiles/Results/ResultsSidebar.js
+++ client/src/components/NewFiles/Results/ResultsSidebar.js
@@ -6,7 +6,6 @@ function ResultsSidebar(props) {
                     <div className="moodSidebarTitle">
                     {/* <img src="" className="moodSidebarImage" alt="..."></img>  */}
                     <img src="https://github.com/MESellassie/MMGB/blob/master/client/public/images/blackPanther.png?raw=true" className="resultCardImage" alt="POSTER"></img>
-
                     </div>
                     {props.children}
                 </div>
